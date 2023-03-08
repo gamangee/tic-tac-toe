7ce3eea2d81483a0202e44e6fdc98903732df404
gamangee <gamangee1991@gmail.com>
2023-03-08 15:50:27 +0900

    calculate winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,11 +4,48 @@ import './Board.css';
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = `Winner : ${winner}`;
+  } else {
+    status = `Next Player ${isNext ? 'X' : 'O'}`;
+  }
 
   const handleClick = (i) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+    if (calculateWinner(newSquares) || newSquares[i]) return;
+    newSquares[i] = isNext ? 'X' : 'O';
     setSquares(newSquares);
+    setIsNext((prev) => !prev);
   };
 
   const renderSquare = (i) => {
@@ -17,7 +54,7 @@ export default function Board() {
 
   return (
     <div>
-      <div className='status'>Next Player : X,O</div>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
